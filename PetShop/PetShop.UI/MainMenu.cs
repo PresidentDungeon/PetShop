f8a88aa320069f68fb70855d1e60c23be13ea0b5
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-08-24 18:25:05 +0200

    Create pet

--- PetShop/PetShop.UI/MainMenu.cs
+++ PetShop/PetShop.UI/MainMenu.cs
@@ -2,6 +2,8 @@
 using PetShop.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 
 namespace PetShop.UI
@@ -9,7 +11,7 @@ namespace PetShop.UI
     public class MainMenu: Menu
     {
         IPetService petService;
-        public MainMenu(IPetService petService) : base("Main Menu", "Show All Pets", "XXX Menu")
+        public MainMenu(IPetService petService) : base("Main Menu", "Show All Pets", "Search Pet", "Add Pet")
         {
             this.petService = petService;
         }
@@ -23,12 +25,77 @@ namespace PetShop.UI
                     Pause();
                     break;
                 case 2:
-
+                    //search
+                    break;
+                case 3:
+                    //AddPet(CreatePet());
+                    CreatePet();
+                    Pause();
                     break;
                 default:
                     break;
             }
         }
+
+        private Pet CreatePet()
+        {
+            Array petTypes = Enum.GetValues(typeof(petType));
+
+            Console.WriteLine("\nEnter pet name:");
+            string petName = Console.ReadLine();
+
+            while (petName.Length <= 0)
+            {
+                Console.WriteLine("\nPlease enter a valid name");
+                petName = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nSelect a pet type");
+
+            for (int i = 0; i < petTypes.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ": " + petTypes.GetValue(i));
+            }
+
+            int selection;
+
+            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > petTypes.Length)
+            {
+                Console.WriteLine($"Invalid input. Please choose an option in range (0-{petTypes.Length})");
+            }
+
+            petType petType = (petType)petTypes.GetValue(selection - 1);
+
+            Console.WriteLine("\nEnter birthdate:");
+            DateTime birthDate;
+
+            while (!DateTime.TryParse(Console.ReadLine(), out birthDate))
+            {
+                Console.WriteLine("Please enter a valid release date (dd/mm/yyyy)");
+            }
+
+            Console.WriteLine("\nEnter pet color:");
+            string petColor = Console.ReadLine();
+
+            while (petColor.Length <= 0)
+            {
+                Console.WriteLine("\nPlease enter a valid color");
+                petColor = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nEnter pet price:");
+            double petPrice;
+
+            while (!double.TryParse(Console.ReadLine(), out petPrice) || petPrice < 0)
+            {
+                Console.WriteLine("\nPlease enter a valid price");
+            }
+
+            Pet pet = new Pet {Name = petName, Type = petType, Birthdate = birthDate, Color = petColor, Price = petPrice};
+            Console.WriteLine(pet);
+            return pet;
+        }
+
 
         private void ShowAllPets()
         {
